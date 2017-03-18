8366c5696b4e24b29ffbc1428e8445a4179ff6e4
Jeffrey Limauro <jlimauro@gmail.com>
2017-03-18 17:22:35 -0400

    Refactored Laser.js to use prototypes.

--- Laser.js
+++ Laser.js
@@ -5,35 +5,35 @@ function Laser(spos, angle) {
   this.pos = createVector(spos.x, spos.y);
   this.vel = p5.Vector.fromAngle(angle);
   this.vel.mult(10);
+}
 
-  this.update = function() {
-    this.pos.add(this.vel);
-  }
-  
-  this.render = function() {
-    push();
-    stroke(255);
-    strokeWeight(4);
-    point(this.pos.x, this.pos.y);
-    pop();
-  }
+Laser.prototype.update = function() {
+  this.pos.add(this.vel);
+}
 
-  this.hits = function(asteroid) {
-    var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
-    if (d < asteroid.r) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+Laser.prototype.render = function() {
+  push();
+  stroke(255);
+  strokeWeight(4);
+  point(this.pos.x, this.pos.y);
+  pop();
+}
 
-  this.offscreen = function() {
-    if (this.pos.x > width || this.pos.x < 0) {
-      return true;
-    }
-    if (this.pos.y > height || this.pos.y < 0) {
-      return true;
-    }
+Laser.prototype.hits = function(asteroid) {
+  var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+  if (d < asteroid.r) {
+    return true;
+  } else {
     return false;
   }
+}
+
+Laser.prototype.offscreen = function() {
+  if (this.pos.x > width || this.pos.x < 0) {
+    return true;
+  }
+  if (this.pos.y > height || this.pos.y < 0) {
+    return true;
+  }
+  return false;
 }
